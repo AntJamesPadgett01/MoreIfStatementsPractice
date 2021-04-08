f7c8b02901e9b9cd3c1e7d574b397557b7ff6825
ArmyAntMF54 <anthonyjpadgett@gmail.com>
2021-04-08 15:31:47 -0500

    Task Done

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace MoreIfStatementsPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetMax(20,10, 40));  
+        }
+        static int GetMax(int num1, int num2, int num3)
+        {
+            int result;
+            if (num1 >= num2 && num1 >= num3) // comparasion  ops <,>,==,<=,>=,==
+            {
+                result = num1;
+            }
+            else if (num2 >= num1 && num2 >= num3)
+            {
+                result = num2;
+            }
+            else
+            {
+                result = num3;
+            }
+
+            return result;
+        }
+
+
+
+
+
+
+
+
+    }
+}
